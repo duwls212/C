0c6d2e67223d8e08f112adbe9ee66edc4e7b3016
duwls212 <108581901+duwls212@users.noreply.github.com>
2023-03-17 14:53:10 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    
+    if(a == b && b == c){
+        printf("%d", 10000 + a * 1000);
+        return 0;
+    }
+    else if(a == b && b != c){
+        printf("%d", 1000 + a * 100);
+        return 0;
+    }
+    else if(a == c && c != b){
+        printf("%d", 1000 + a * 100);
+        return 0;
+    }
+    else if(c == b && a != c){
+        printf("%d", 1000 + b * 100);
+        return 0;
+    }
+    
+    if(a > b && a > c){
+        printf("%d", a * 100);
+    }
+    else if(b > a && b > c){
+        printf("%d", b * 100);
+    }
+    else if(c > b && c > a){
+        printf("%d", c * 100);
+    }
+}
